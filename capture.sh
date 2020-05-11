fa8c0f9f354eb198b32da0436cb34efc02b3ae3a
vzvu3k6k <vzvu3k6k@gmail.com>
2020-05-12 05:55:11 +0900

    Specify uid for permission

--- capture.sh
+++ capture.sh
@@ -5,6 +5,7 @@ set -eux
 docker build . -t osaka-covid-19-signal:latest
 
 docker container run --rm \
+  -u $(id -u) \
   -v $(pwd)/capture:/usr/src/app/capture \
   --security-opt seccomp=chrome.json \
   osaka-covid-19-signal:latest node capture/main.js
