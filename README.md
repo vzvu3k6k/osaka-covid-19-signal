348afe61fa322ac919f661941a6e2eea3b076d01
vzvu3k6k <vzvu3k6k@gmail.com>
2020-05-12 04:42:56 +0900

    Enable Chromium sandbox with seccomp option
    chrome.json is taken from
    https://github.com/Zenika/alpine-chrome/blob/master/chrome.json.

---
+++ README.md
@@ -0,0 +1,1 @@
+`chrome.json` is taken from <https://github.com/Zenika/alpine-chrome/blob/master/chrome.json>.
--- capture.sh
+++ capture.sh
@@ -2,4 +2,5 @@
 
 docker container run --rm \
   -v $(pwd)/capture:/usr/src/app/capture \
+  --security-opt seccomp=chrome.json \
   zenika/alpine-chrome:81-with-puppeteer node capture/main.js
