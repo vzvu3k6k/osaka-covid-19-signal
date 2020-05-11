aa6a7223ff4cc0c28eabd4ffe5275e0a9c22a835
vzvu3k6k <vzvu3k6k@gmail.com>
2020-05-12 05:17:26 +0900

    Hide link of "大阪モデルの詳細はこちらへ"

--- capture/main.js
+++ capture/main.js
@@ -4,7 +4,14 @@ const puppeteer = require('puppeteer');
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto('http://www.pref.osaka.lg.jp/default.html');
+
+  // 「大阪モデルの詳細はこちらへ」のセルを非表示にする
+  await page.addStyleTag({
+    content: '[style="text-align:right;margin-top:5px;margin-bottom:5px;"] { display: none }'
+  });
+
   const $signalBox = await page.$('.shingo-waku');
   await $signalBox.screenshot({ path: './capture/screenshots/signal.png' });
+
   await browser.close();
 })();
