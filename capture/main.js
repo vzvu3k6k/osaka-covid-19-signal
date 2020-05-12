825a618f65c3b229733d3a94cf64270adeebf35f
vzvu3k6k <vzvu3k6k@gmail.com>
2020-05-13 04:24:31 +0900

    Hide "▼ 信号の意味についてはこちら"

--- capture/main.js
+++ capture/main.js
@@ -5,6 +5,11 @@ const puppeteer = require('puppeteer');
   const page = await browser.newPage();
   await page.goto('http://www.pref.osaka.lg.jp/default.html');
 
+  // 「▼ 信号の意味についてはこちら」を非表示にする
+  await page.addStyleTag({
+    content: '.shingo-waku [style="cursor:pointer;"] { display: none }'
+  });
+
   // 「大阪モデルの詳細はこちらへ」のセルを非表示にする
   await page.addStyleTag({
     content: '[style="text-align:right;margin-top:5px;margin-bottom:5px;"] { display: none }'
